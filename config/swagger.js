c2fbb1bf1e39df6e108b2336b8c5d9199e092e71
maxmat2501 <blouin.maxence.pro@gmail.com>
2025-07-04 14:08:44 +0200

    fix servers url doc swagger

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api/v1',
+        url: 'https://carambar-api-0cqu.onrender.com/api/v1',
       },
     ],
   },
